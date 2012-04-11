ff1a6034605ca00026b775c363eeedfe38ddee31
casey <casey@gigaom.com>
2012-04-11 06:07:12 +0000

    added the boostrapper
    git-svn-id: http://plugins.svn.wordpress.org/bcms/trunk@529727 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bcms.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: bCMS: bSuite CMS Tools
+Plugin URI: http://maisonbisson.com/bsuite/
+Version: a1
+Author: Casey Bisson
+Author URI: http://maisonbisson.com/blog/
+*/
+
+
+// include required components
+require_once( dirname( __FILE__ ) .'/components/innerindex.php');
