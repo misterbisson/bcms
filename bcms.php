c576a6abbc9c71c3783f69bbf1f137de258c97ff
casey <casey@gigaom.com>
2012-04-14 17:44:25 +0000

    breaking the cms widgets file into smaller pieces
    git-svn-id: http://plugins.svn.wordpress.org/bcms/trunk@531109 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bcms.php
+++ bcms.php
@@ -7,16 +7,21 @@ Author: Casey Bisson
 Author URI: http://maisonbisson.com/blog/
 */
 
+// the admin menu
 if ( is_admin() )
 	require_once dirname( __FILE__ ) . '/admin.php';
 
-// include required components
-require_once( dirname( __FILE__ ) .'/components/cms-widgets.php' );
+// include the core components
+require_once( dirname( __FILE__ ) .'/components/postloops.php' );
 require_once( dirname( __FILE__ ) .'/components/wijax.php' );
+require_once( dirname( __FILE__ ) .'/components/late-enqueue.php' );
+
+// include the CMS convenience features
+require_once( dirname( __FILE__ ) .'/components/cms-widgets.php' );
 require_once( dirname( __FILE__ ) .'/components/innerindex.php');
 require_once( dirname( __FILE__ ) .'/components/listchildren.php' );
 require_once( dirname( __FILE__ ) .'/components/privacy.php' );
 
-// full text indexing in mysql
+// optionally include the mysql-based full text indexing
 if( get_option('bcms_searchsmart'))
 	require_once( dirname( __FILE__ ) .'/components/search.php' );
---
+++ components/late-enqueue.php
@@ -0,0 +1,69 @@
+<?php
+
+
+function bcms_late_enqueue_script( $handle, $src = false, $deps = array(), $ver = false, $in_footer = false )
+{
+	global $wp_scripts;
+
+	// enqueue the named script
+	wp_enqueue_script( $handle , $src , $deps , $ver , $in_footer );
+
+	// resolve dependencies and place everything in the array of items to put in the footer
+	$to_do_orig = (array) $wp_scripts->to_do;
+	$wp_scripts->all_deps( array( $handle ));
+	$wp_scripts->in_footer = array_merge( (array) $wp_scripts->in_footer , (array) array_diff( (array) $wp_scripts->to_do , $to_do_orig ) );
+}
+
+function bcms_late_enqueue_style( $handle, $src = false, $deps = array(), $ver = false, $media = 'all' )
+{
+	global $wp_styles;
+
+	// enqueue the named script
+	wp_enqueue_style( $handle, $src, $deps, $ver, $media );
+
+	// resolve dependencies and place everything in the array of items to put in the footer
+	$to_do_orig = (array) $wp_styles->to_do;
+	$wp_styles->all_deps( array( $handle ));
+	$wp_styles->in_footer = array_merge( (array) $wp_styles->in_footer , (array) array_diff( (array) $wp_styles->to_do , $to_do_orig ) );
+
+	add_filter( 'print_footer_scripts', 'bcms_print_late_styles' );
+}
+
+function bcms_print_late_styles()
+{
+	global $wp_styles;
+
+	$tags = array();
+	foreach( (array) $wp_styles->to_do as $handle )
+	{
+		if ( isset($wp_styles->registered[$handle]->args) )
+			$media = esc_attr( $wp_styles->registered[$handle]->args );
+		else
+			$media = 'all';
+
+		$href = $wp_styles->_css_href( $wp_styles->registered[$handle]->src, $ver, $handle );
+		$rel = isset($wp_styles->registered[$handle]->extra['alt']) && $wp_styles->registered[$handle]->extra['alt'] ? 'alternate stylesheet' : 'stylesheet';
+		$title = isset($wp_styles->registered[$handle]->extra['title']) ? "title='" . esc_attr( $wp_styles->registered[$handle]->extra['title'] ) . "'" : '';
+
+		$tags[] = "$('head').append(\"<link rel='$rel' id='$handle-css' $title href='$href' type='text/css' media='$media' />\");\n";
+	}		
+
+	if( ! array( $tags ))
+		return;
+
+?>
+<script type="text/javascript">	
+	;(function($){
+		$(window).load(function(){
+			// print the style includes
+<?php foreach( $tags as $tag )
+{
+	echo "			$tag";
+} ?>
+
+		});
+	})(jQuery);
+</script>
+<?php
+}
+
