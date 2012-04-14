04cabe0ae6b431ef1f0b79cd62aa775c852d757f
casey <casey@gigaom.com>
2012-04-14 22:37:30 +0000

    corrected version number
    git-svn-id: http://plugins.svn.wordpress.org/bcms/trunk@531195 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bcms.php
+++ bcms.php
@@ -3,7 +3,7 @@
 Plugin Name: bCMS: bSuite CMS Tools
 Plugin URI: http://maisonbisson.com/bsuite/
 Description: Make WordPress a better CMS. Create a post loop in a widget. Lazy load widgets. More shortcodes. More good.
-Version: a1
+Version: 5.0
 Author: Casey Bisson
 Author URI: http://maisonbisson.com/blog/
 */
