97924c7028def4de2c009297fd294e0d87c8fbb2
Matthew Batchelder <borkweb@gmail.com>
2013-02-08 08:37:10 -0500

    Add better comments to bcms-wiframe

--- components/js/bcms-wiframe.js
+++ components/js/bcms-wiframe.js
@@ -1,9 +1,18 @@
+// define bcms if another script hasn't already done so
 if ( typeof bcms == "undefined" ) {
 	var bcms = {
 		wiframes: 1
 	};
 }//end if
 
+// define the wiframes variable if it isn't defined
+if ( typeof bcms.wiframes == "undefined" ) {
+	bcms.wiframes = 1;
+}//end if
+
+/**
+ * initialize a wiframe
+ */
 bcms.wiframe = function( args ) {
 	bcms.args = args;
 
@@ -17,32 +26,44 @@ bcms.wiframe = function( args ) {
 		document.write( unescape( '%3Cscript src="' + bcms.args.url.replace( /wiframe\/.*/, '') + 'wp-content/plugins/bcms/components/js/jquery.ba-postmessage.min.js"%3E%3C/script%3E' ) );
 	}//end if
 
-	bcms.load_libraries();
+	bcms.libraries_loaded();
 };
 
-bcms.load_libraries = function() {
+/**
+ * check if the needed libraries have loaded.
+ */
+bcms.libraries_loaded = function() {
 	if ( typeof jQuery === "undefined" || typeof jQuery.receiveMessage === "undefined" ) {
 		setTimeout( bcms.load_libraries, 10 );
 		return;
 	}//end if
 
+	// once the libraries are loaded, we can render the iframe
 	bcms.render_iframe();
 };
 
+/**
+ * render the iframe
+ */
 bcms.render_iframe = function() {
 	var src = bcms.args.url;
 	var the_id = 'bcms-wiframe-' + bcms.wiframes;
 
+	// append a dummy variable so concatenation is easier
 	if ( ! src.match( /\?/ ) ) {
 		src += '?_';
 	}//end if
 
+	// append the arguments to the URL
 	for ( var key in bcms.args ) {
-		src += '&' + key + '=' + escape( bcms.args[ key ] );
+		if ( 'url' !== key ) {
+			src += '&' + key + '=' + escape( bcms.args[ key ] );
+		}//end if
 	}//end for
 
 	src = src + '#' + encodeURIComponent( document.location.href );
 
+	// increment the wiframe tracker so our IDs are unique
 	bcms.wiframes++;
 
 	var iframe = '%3Ciframe id="' + the_id + '" width="100%" height="300" scrolling="no" frameborder="0" src="' + src + '"%3E%3C/iframe%3E';
@@ -52,6 +73,7 @@ bcms.render_iframe = function() {
 
 	var height = $frame.outerHeight();
 
+	// watch for messages from the iframe to adjust the height
 	jQuery.receiveMessage(function(e){
 		// Get the height from the passsed data.
 		var h = Number( e.data.replace( /.*bcms_wiframe_height=(\d+)(?:&|$)/, '$1' ) );
