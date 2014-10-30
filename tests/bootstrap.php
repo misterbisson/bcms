69bf2ee1c4ede0bdc36dc658f2e97621b47fbb33
Casey Bisson <casey.bisson@gmail.com>
2014-10-30 09:57:06 -0700

    refactoring tests for travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,32 +1,17 @@
 <?php
-// Load WordPress test environment
-// https://github.com/nb/wordpress-tests
-//
 
-// get the path to wordpress-tests' bootstrap.php file from the environment
-$bootstrap = getenv( 'WPT_BOOTSTRAP' );
-if ( FALSE === $bootstrap )
-{
-	echo "\n!!! Please set the WPT_BOOTSTRAP env var to point to your\n!!! wordpress-tests/includes/bootstrap.php file.\n\n";
-	return;
+$_tests_dir = getenv('WP_TESTS_DIR');
+if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+
+// set the options to activate the plugin
+passthru("wp option add 'bcms_searchsmart' '1'");
+
+require_once $_tests_dir . '/includes/functions.php';
+
+function _manually_load_plugin() {
+	require dirname( __FILE__ ) . '/../bcms.php';
 }
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-if ( file_exists( $bootstrap ) )
-{
-	$GLOBALS['wp_tests_options'] = array(
-		// set 'pro' to TRUE to use GO pro style WP directory layout.
-		// unset it to use standard WP installation layout
-		'pro' => TRUE,
-		'active_plugins' => array(
-			'bcms/bcms.php',
-		),
-	);
+require $_tests_dir . '/includes/bootstrap.php';
 
-	require_once $bootstrap;
-
-	update_option( 'bcms_searchsmart', TRUE );
-}//end if
-else
-{
-	exit( "Couldn't find path to wordpress-tests/bootstrap.php\n" );
-}//end else
